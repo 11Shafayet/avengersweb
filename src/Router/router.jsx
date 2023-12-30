720b4b8dff85908a2eb0c734509d92eda1bfc77e
Shafayet <11shafayet@gmail.com>
2023-12-30 11:10:14 +0600

    starter pack

---
+++ src/Router/router.jsx
@@ -0,0 +1,77 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../MainLayout/MainLayout';
+
+import Home from '../pages/Home';
+import AllBlogs from '../pages/AllBlogs';
+import Contact from '../pages/Contact';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+// admin routes
+import AdminLayout from '../MainLayout/AdminLayout';
+import MyBlogs from '../admin/pages/MyBlogs';
+import AddBlog from '../admin/pages/AddBlog';
+import AllUsers from '../admin/pages/AllUsers';
+import PrivateRoute from './PrivateRoute';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/allblogs',
+        element: <AllBlogs />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+  // admin routes
+  {
+    path: '/dashboard',
+    element: <AdminLayout />,
+    children: [
+      {
+        path: '/dashboard',
+        element: (
+          <PrivateRoute>
+            <MyBlogs />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/dashboard/add-blog',
+        element: (
+          <PrivateRoute>
+            <AddBlog />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/dashboard/all-users',
+        element: (
+          <PrivateRoute>
+            <AllUsers />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import logo from '/images/logo.png';
+
+const Navbar = () => {
+  const { activeNav, setActiveNav } = useAuth();
+  const [isUser, setIsUser] = useState(null);
+  const [activeProfile, setActiveProfile] = useState(false);
+
+  useEffect(() => {
+    const userExists = JSON.parse(localStorage.getItem('userInfo'));
+
+    setIsUser(userExists);
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('userInfo');
+    setIsUser(null);
+  };
+
+  const navData = [
+    {
+      title: 'Home',
+      text: 'home',
+      link: '/',
+    },
+    {
+      title: 'All Blogs',
+      text: 'allblogs',
+      link: '/allblogs',
+    },
+    {
+      title: 'Featured',
+      text: 'featured',
+      link: '/allblogs',
+    },
+    {
+      title: 'Contact',
+      text: 'contact',
+      link: '/contact',
+    },
+  ];
+
+  return (
+    <>
+      <div className="min-h-20 flex justify-center items-center py-4">
+        <img src={logo} alt="Logo" className="h-16 object-cover" />
+      </div>
+
+      <div className="bg-primary text-white font-primary w-full">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-center">
+            <ul className="flex text-sm font-semibold">
+              {navData.map((item, i) => (
+                <Link to={item.link} key={i}>
+                  <li
+                    className={`uppercase hover:bg-black hover:bg-opacity-15 py-4 px-5 duration-500 cursor-pointer ${
+                      activeNav === item.text && 'bg-black bg-opacity-15'
+                    }`}
+                    onClick={() => setActiveNav(item.text)}
+                  >
+                    {item.title}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+            <div className="flex items-center">
+              {!isUser ? (
+                <>
+                  <Link
+                    to="/login"
+                    className={`uppercase hover:bg-black hover:bg-opacity-15 py-4 px-5 duration-500 cursor-pointer ${
+                      activeNav === 'login' && 'bg-black bg-opacity-15'
+                    }`}
+                    onClick={() => {
+                      setActiveNav('login');
+                    }}
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to="/register"
+                    className={`uppercase hover:bg-black hover:bg-opacity-15 py-4 px-5 duration-500 cursor-pointer ${
+                      activeNav === 'register' && 'bg-black bg-opacity-15'
+                    }`}
+                    onClick={() => {
+                      setActiveNav('register');
+                    }}
+                  >
+                    Register
+                  </Link>
+                </>
+              ) : (
+                <div
+                  className="relative bg-gray-100 rounded-full cursor-pointer"
+                  onClick={() => setActiveProfile((prev) => !prev)}
+                >
+                  <img
+                    src={isUser?.pic}
+                    alt=""
+                    className="w-11 h-11 rounded-full"
+                  />
+                  {activeProfile && (
+                    <div className="absolute bg-white text-black top-full right-0 rounded-md z-[100] pt-4 shadow">
+                      <Link
+                        to={`/dashboard`}
+                        className="uppercase hover:bg-black hover:bg-opacity-15 py-4 px-5 duration-500 font-bold cursor-pointer"
+                      >
+                        Dashboard
+                      </Link>
+                      <h4
+                        className="uppercase hover:bg-black hover:bg-opacity-15 py-4 px-5 duration-500 font-bold cursor-pointer"
+                        onClick={logoutHandler}
+                      >
+                        Logout
+                      </h4>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
